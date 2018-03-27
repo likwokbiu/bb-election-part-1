1f35f2bfe533ce42cce54466a7a0a857aaa33c78
likwokbiu <li_bill@ymail.com>
2018-03-27 13:19:22 -0400

    get and show the election results

--- election.js
+++ election.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData) {
+    var ul = document.getElementById('election_results');
+    for (var i = 0; i < responseData.candidates.length; i++) {
+      console.log(responseData.candidates[i]);
+      var li = document.createElement("li");
+      li.innerText = responseData.candidates[i].name + " has " + responseData.candidates[i].votes + " votes.";
+      ul.appendChild(li);
+    };
+  });
 
 });
